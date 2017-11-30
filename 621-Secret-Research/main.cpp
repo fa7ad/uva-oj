efd6da7ef80762e0e68b0e56152ebccd6498be46
Fahad Hossain <evilinfinite7@gmail.com>
2017-11-30 20:13:31 +0600

    format some more

--- 621-Secret-Research/main.cpp
+++ 621-Secret-Research/main.cpp
@@ -9,10 +9,10 @@ int main() {
   while (cnum--) {
     scanf("%s", s);
 
-    int len = strlen(s);
+    int end = strlen(s) - 1;
     if (!strcmp(s, "1") || !strcmp(s, "4") || !strcmp(s, "78")) printf("+\n");
-    else if (s[len - 1] == '5' && s[len - 2] == '3') printf("-\n");
-    else if (s[0] == '9' && s[len - 1] == '4') printf("*\n");
+    else if (s[end] == '5' && s[end - 1] == '3') printf("-\n");
+    else if (s[0] == '9' && s[end] == '4') printf("*\n");
     else if (s[0] == '1' && s[1] == '9' && s[2] == '0') printf("?\n");
     else printf("+\n");
   }
